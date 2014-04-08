d29e9caa30913874116d72d95153404ab2977797
epurcell3 <epurcell3@gatech.edu>
2014-04-08 15:48:40 -0400

    Added a little robot to move around for testing.
    Remove later.

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+    public Vector2 speed = new Vector2(10,10);
+
+    private Vector2 movement;
+	
+	// Update is called once per frame
+	void Update () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+}
