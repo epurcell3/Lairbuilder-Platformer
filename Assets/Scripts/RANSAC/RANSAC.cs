dc0a45520521a2a9eb6425d99deb15f1cbf8c7a2
epurcell3 <epurcell3@gatech.edu>
2014-04-10 10:01:09 -0400

    RANSAC algo.
    Needs to pick neighbors better. Gets ArgumentOutOfRangeExceptions.

---
+++ Assets/Scripts/RANSAC/RANSAC.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class RANSAC {
+    private static int N = 100;
+    private static int S = 3;
+    private static float D = 10;
+    private static float X = 10;
+    private static int C = 10;
+
+    private static System.Random random = new System.Random();
+
+    public static List<Line> run(List<Vector2> points, float sep_degree)
+    {
+        List<Line> lines = new List<Line>();
+        int n_tires = 0;
+
+        while (!(points.Count == 0) && n_tires < N)
+        {
+            int i = random.Next(points.Count);
+            List<Vector2> samples = new List<Vector2>();
+            samples.Add(points[i]);
+            for (int c = 0; c < S; c++)
+            {
+                Vector2 sample;
+                int range = (int)(D / sep_degree);
+                try
+                {
+                    if (random.Next(2) == 0)
+                        sample = points[i + random.Next(range + 1)];
+                    else
+                        sample = points[i - random.Next(range + 1)];
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    c--;
+                    continue;
+                }
+                if (!(samples.Contains(sample)))
+                {
+                    samples.Add(sample);
+                }
+                else
+                {
+                    c--;
+                }
+            }
+            float m, b;
+            LLS(samples, out m, out b);
+            foreach (Vector2 point in points)
+            {
+                if(samples.Contains(point))
+                    continue;
+                if (distance(m, b, point) <= X)
+                    samples.Add(point);
+            }
+            if (samples.Count > C)
+            {
+                LLS(samples, out m, out b);
+                lines.Add(new Line(new Vector2(0, b), new Vector2(1, m)));
+                foreach (Vector2 sample in samples)
+                {
+                    points.Remove(sample);
+                }
+            }
+
+        }
+
+        return lines;
+    }
+
+    static void LLS(List<Vector2> points, out float m, out float b)
+    {
+        float x, y, xy, x2, J;
+
+        x = y = xy = x2 = 0.0f;
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            x += points[i].x;
+            y += points[i].y;
+            xy += points[i].x * points[i].y;
+            x2 += points[i].x * points[i].x;
+        }
+
+        J = ((float)points.Count * x2) - (x * x);
+        if (J != 0.0)
+        {
+            m = (((float)points.Count * xy) - (x * y)) / J;
+            m = Convert.ToSingle(Math.Floor(1.0E3 * m + 0.5) / 1.0E3);
+            b = ((y * x2) - (x * xy)) / J;
+            b = Convert.ToSingle(Math.Floor(1.0E3 * b + 0.5) / 1.0E3);
+        }
+        else
+        {
+            m = 0.0f;
+            b = 0.0f;
+        }
+    }
+
+    static double distance(double m, double b, Vector2 point)
+    {
+        return Math.Abs(m * point.x + 1 * point.y + b) / Math.Sqrt(m * m + 1 * 1);
+    }
+
+    public static void seed(int seed)
+    {
+        random = new System.Random(seed);
+    }
+
+    public class Line
+    {
+        private Vector2 point;
+        private Vector2 d;
+
+        public Line(Vector2 point, Vector2 d)
+        {
+            this.point = point;
+            this.d = d;
+        }
+
+        public Vector2 Point
+        {
+            get { return point; }
+        }
+        
+        public Vector2 D 
+        {
+            get { return d; }
+        }
+    }
+
+}
---
+++ Assets/Scripts/RANSAC/RANSACTest.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class RANSACTest : MonoBehaviour {
+
+    List<RANSAC.Line> lines;
+
+    public bool showRays = false;
+    public bool showLines = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        List<Vector2> points = new List<Vector2>();
+        double fov = 90;
+        float distance = 1;
+        for (double i = -fov/2; i < fov/2; i += 1)
+        {
+            Ray ray = new Ray(transform.position, new Vector2(Convert.ToSingle(Math.Cos(i * Math.PI / 180)), Convert.ToSingle(Math.Sin(i * Math.PI / 180))));
+            //Debug.DrawRay(transform.position, new Vector2(Convert.ToSingle(Math.Cos(i * 2 * Math.PI / 180)), Convert.ToSingle(Math.Sin(i * 2 * Math.PI / 180))));
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(Convert.ToSingle(Math.Cos(i * Math.PI / 180)), Convert.ToSingle(Math.Sin(i * Math.PI / 180))), distance, 1);
+            if (hit != null && hit.point != new Vector2(0,0))
+            {
+                points.Add(hit.point);
+                Debug.Log(hit.point);
+                if (showRays)
+                    Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            }
+            else
+            {
+                if (showRays)
+                {
+                    Vector2 end = new Vector2(transform.position.x + Convert.ToSingle(Math.Cos(i * Math.PI / 180)) * distance, transform.position.y + Convert.ToSingle(Math.Sin(i * Math.PI / 180)) * distance);
+                    Debug.DrawLine(transform.position, end, Color.green);
+                }
+            }
+        }
+
+        lines = RANSAC.run(points, 1);
+
+        foreach (RANSAC.Line line in lines)
+        {
+            if (showLines)
+            {
+                Debug.DrawRay(line.Point, line.D);
+            }
+        }
+
+	}
+}
