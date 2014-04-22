14304724cce861e9517981734bb4a2eb890d0773
chraxia <csmythe92@gmail.com>
2014-04-22 10:13:48 -0400

    Stage 1 RANSAC
    Stage 1 handles RANSAC now. Used for demo.

--- Assets/Scripts/RANSAC/RANSAC.cs
+++ Assets/Scripts/RANSAC/RANSAC.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System;
 
-public class RANSAC {
+public class RANSAC : MonoBehaviour{
     private static int N = 100;
     private static int S = 3;
     private static float D = 10;
